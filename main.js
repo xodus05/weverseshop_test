75a217d587bd91584ae02d5bcdf6e3c4faa029ea
xodus05 <s2121@e-mirim.hs.kr>
2022-12-02 02:02:20 +0900

    헤더 거의 완성

---
+++ main.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    $(function() {
+        $('li').click(function(){
+            $('li').removeClass()
+                $(this).addClass('on')
+        })
+    })
+</script>
